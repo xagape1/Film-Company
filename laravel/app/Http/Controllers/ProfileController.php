138a0a5b5c5931579643d5c299a2027897a08138
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-05-05 20:47:21 +0200

    prueba

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\Profile;
 
 class ProfileController extends Controller
--- laravel/app/Models/Profile.php
+++ laravel/app/Models/Profile.php
@@ -9,6 +9,8 @@ class Profile extends Model
 {
     use HasFactory;
 
+    protected $table = 'profiles';
+
     protected $fillable = [
         'id_users',
         'name',
