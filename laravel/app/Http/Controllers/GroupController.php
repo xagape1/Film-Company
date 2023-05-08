715a80f84f17a15e836ec2a1d139b9d2e7ddd0f0
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-05-08 16:32:20 +0200

    groups

--- laravel/app/Http/Controllers/GroupController.php
+++ laravel/app/Http/Controllers/GroupController.php
@@ -14,8 +14,8 @@ class GroupController extends Controller
      */
     public function index()
     {
-        $groups = Group::all();
-        return response()->json($groups);
+        $group = Group::all();
+        return response()->json($group);
     }
 
     /**
