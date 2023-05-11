542a1ea3b11b0200b2f55251fb19d16ae5a62fc2
xagape1 <xagape@fp.insjoaquimmir.cat>
2023-05-11 15:50:32 +0200

    por

--- laravel/app/Http/Controllers/CollectionController.php
+++ laravel/app/Http/Controllers/CollectionController.php
@@ -5,85 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\Collection;
 use Illuminate\Http\Request;
 
-class CollectionApiController extends Controller
+class CollectionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $collections = Collection::all();
-
-        return response()->json([
-            'data' => $collections
-        ]);
+        $collections = Collection::where('id_profile', $request->id_profile)->get();
+        return response()->json(['collections' => $collections]);
     }
-
+    public function create(Request $request)
+    {
+        return response()->json(['collection' => new Collection(['id_profile' => $request->id_profile])]);
+    }
+    
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|max:255',
-            'movies' => 'array',
-            'episodes' => 'array',
-        ]);
-
-        $collection = new Collection();
-        $collection->name = $request->input('name');
+        $collection = new Collection;
+        $collection->name = $request->name;
+        $collection->id_profile = $request->id_profile;
         $collection->save();
-
-        if ($request->has('movies')) {
-            $collection->movies()->attach($request->input('movies'));
-        }
-
-        if ($request->has('episodes')) {
-            $collection->episodes()->attach($request->input('episodes'));
-        }
-
-        return response()->json([
-            'message' => 'Collection created successfully.',
-            'data' => $collection
-        ], 201);
+        return response()->json(['message' => 'Collection created', 'collection' => $collection]);
     }
 
-    public function show(Collection $collection)
+    public function show($id)
     {
-        return response()->json([
-            'data' => $collection
-        ]);
+        $collection = Collection::find($id);
+        return response()->json(['collection' => $collection]);
     }
 
-    public function update(Request $request, Collection $collection)
+    public function update(Request $request, $id)
     {
-        $request->validate([
-            'name' => 'required|max:255',
-            'movies' => 'array',
-            'episodes' => 'array',
-        ]);
-
-        $collection->name = $request->input('name');
+        $collection = Collection::find($id);
+        $collection->name = $request->name;
+        $collection->id_profile = $request->id_profile;
         $collection->save();
-
-        $collection->movies()->detach();
-        if ($request->has('movies')) {
-            $collection->movies()->attach($request->input('movies'));
-        }
-
-        $collection->episodes()->detach();
-        if ($request->has('episodes')) {
-            $collection->episodes()->attach($request->input('episodes'));
-        }
-
-        return response()->json([
-            'message' => 'Collection updated successfully.',
-            'data' => $collection
-        ]);
+        return response()->json(['message' => 'Collection updated', 'collection' => $collection]);
     }
 
-    public function destroy(Collection $collection)
+    public function destroy($id)
     {
-        $collection->movies()->detach();
-        $collection->episodes()->detach();
+        $collection = Collection::find($id);
         $collection->delete();
-
-        return response()->json([
-            'message' => 'Collection deleted successfully.'
-        ]);
+        return response()->json(['message' => 'Collection deleted']);
     }
 }
